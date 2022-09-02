26efd19af2616a6848d67979bb2e749f7ae17114
shafayat zamil <zamilkhan111.za@gmail.com>
2022-09-02 23:14:20 +0600

    news box updated

--- js/index.js
+++ js/index.js
@@ -27,7 +27,18 @@ const loadData= async (id)=>{
 const displayData=(catagories)=>{
     console.log(catagories);
     catagories.forEach(element => {
-        
+        const newsBox= document.getElementById('news-box');
+        const newsDiv= document.createElement('div');
+        newsDiv.classList.add('col');
+        newsDiv.innerHTML=`
+        <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        `
+        newsBox.appendChild(newsDiv);
     });
 }
 
